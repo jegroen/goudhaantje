7e4e0b3cc41ee8bfce287f92b635678c6d0c574a
Jeroen Jansen <jjansen@MacBook-Pro-van-Jeroen.local>
2016-11-28 16:50:16 +0100

    GOUDHAANTJE-4 and -3: Assortiment type and page added.

--- wp-content/themes/twentythirteen-child/posts_loop_template.php
+++ wp-content/themes/twentythirteen-child/posts_loop_template.php
@@ -1,15 +1,10 @@
-<div class="location-post">
-    <h3 class="entry-title"><?php the_title(); ?></h3>
+<?php
+    if ( in_category('Standplaatsen') ) {
+        include 'single-standplaats.php';
+    } elseif ( in_category('Assortiment-item') ) {
+        include 'single-assortiment-item.php';
+    }
+?>
 
-    <div class="entry-summary">
-        <?php the_excerpt(); ?>
-    </div>
-    <div class="location-title">
-        <?php echo get_post_meta( get_the_ID(), 'standplaats-titel', true); ?>
-    </div>
-    <div class="location-description">
-        <?php echo get_post_meta( get_the_ID(), 'standplaats-omschrijving', true); ?>
-    </div>
-</div>
 
 
